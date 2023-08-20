42354bcf494d00c99a35d7bbdd934252ae332d8e
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2023-08-20 21:28:25 +0700

    Add document icon for activities page

--- src/components/activities/ActivitiesGrid.vue
+++ src/components/activities/ActivitiesGrid.vue
@@ -5,7 +5,15 @@
     @select="openDoc"
     @select-mobile="clickMobile"
     @download="openDoc"
-  />
+  >
+    <svg-icon 
+      :path="mdiFileDocument"
+      type="mdi"
+      size="48"
+      style="margin: 5px;"
+    />
+    <h3>Activities Document</h3>
+  </StandardGrid>
   <h2>Games</h2>
   <StandardGrid
     :items="games"
@@ -17,6 +25,9 @@ import StandardGrid from "../cardgrid/StandardGrid.vue";
 import jsonData from '../../data.json';
 import { YoutubeData } from "../../types";
 import { useRouter } from "vue-router";
+import { mdiFileDocument } from '@mdi/js';
+// @ts-ignore
+import SvgIcon from '@jamescoyle/vue-icon';
 
 const games = (jsonData as YoutubeData).activities.games.map(game => ({
   title: game.gameTitle.split(" ").map(word => word[0].toUpperCase() + word.slice(1)).join(" "),
